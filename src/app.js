261cba4d22122376e214c285b0d72c9523b0bed0
isabellafrancog <isabellagamboa1@gmail.com>
2022-08-22 13:47:52 -0400

    added some more css and the api and js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
+function displayTemperature(response) {
+  console.log(response);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  console.log(response);
+}
+let apiKey = "9db7eb713944807071b9838915b210c1";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
